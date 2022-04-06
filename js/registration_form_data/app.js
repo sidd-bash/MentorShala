8fc5fb7ae131314fb212ae54cca23e240e04cfd3
Abhishek <abhiofficial581@gmail.com>
2022-04-06 07:10:08 +0530

    serving html files through express, successfully done

--- js/registration_form_data/app.js
+++ js/registration_form_data/app.js
@@ -7,19 +7,18 @@ const fs=require('fs');
 const { send } = require('process');
 const { Script } = require('vm');
 // const { hostname } = require('os');
-// const path_file=path.join(__dirname,"../")
 
-// console.log(path.join(__dirname,"../../HTML"));
-// staticpath=path.join(__dirname,"../../HTML")
+
+
 // app.use(express.static(staticpath))
-app.set('view engine','pug');//Set the template Engine as pug
-app.set('views',path.join(__dirname,'views'));//Set the views directory
+// app.set('view engine','pug');
+// app.set('views',path.join(__dirname,'views'));
+// app.use(express.static(__dirname + "/../../CSS"));
 
 // app.use('/static',express.static('static'));//for serving static files
 const mongoose = require('mongoose');
 const bodyparser=require('body-parser')
 main().catch(err => console.log(err));
-
 
 async function main() {
     await mongoose.connect('mongodb://localhost:27017/registered_User_info');    //mentor_info is my database name
@@ -38,10 +37,10 @@ const mentor_registrator = new Schema({
     userName:   String,
     city: String,
     educationInfo: String,
-    preferedLanguage: String,
+    preferdlang: String,
     skills: String,
     shortDescription: String,
-  });
+});
 
 const mentor_register = mongoose.model('mentor_registration',mentor_registrator );     //mentor_registration is collection name
 const mentee_registrator = new Schema({
@@ -53,22 +52,25 @@ const mentee_registrator = new Schema({
     preferedLanguage: String,
     Interest: String,
     shortDescription: String,
-  });
+});
 
 const mentee_register = mongoose.model('mentee_registration',mentee_registrator );     //mentor_registration is collection name
-app.use('/static',express.static('static'));
-app.use(express.urlencoded());
-app.set('view engine','pug');
-app.set('views',path.join(__dirname,'views'));
+// app.use('/static',express.static('static'));
+// app.use(express.urlencoded());
+// app.set('view engine','pug');
 
+
+app.use(express.static(path.join(__dirname,"../../")));
 app.get('/HTML/mentor-registration.html',(req,res)=>{ 
-    // const mentor_registration_file=fs.readFileSync('../../HTML/mentor-registration.html');
-    // res.writeHead(200,{'Content-Type':'text/html'});
-    // res.end(mentor_registration_file)
-    res.set({
-        'Access-control-Allow-Origin': '*'
-    });
-    return res.redirect('rgistration.html');
+    const mentor_registration_file=fs.readFileSync('../../HTML/mentor-registration.html');
+    res.writeHead(200,{'Content-Type':'text/html'});
+    res.end(mentor_registration_file)
+    // res.sendFile(path.join(__dirname,"public","mentor-registration.html"));
+
+    // res.set({
+    //     'Access-control-Allow-Origin': '*'
+    // });
+    // res.redirect('rgistration.html');
     // res.status(200).render('mentor_registration.pug');
 })
 app.get('/HTML/mentee-registration.html',(req,res)=>{ 
@@ -77,33 +79,48 @@ app.get('/HTML/mentee-registration.html',(req,res)=>{
     res.end(mentee_registration_file)
 })   
 app.get('/index.html',(req,res)=>{ 
-    const Home=fs.readFileSync('../../index.html');
+    const Home=fs.readFileSync('index.html');
+    res.writeHead(200,{'Content-Type':'text/html'});
+    res.end(Home)
+})
+app.get('/HTML/login-mentor.html',(req,res)=>{ 
+    const Home=fs.readFileSync('../../HTML/login-mentor.html');
+    res.writeHead(200,{'Content-Type':'text/html'});
+    res.end(Home)
+})
+app.get('HTML/login-mentee.html',(req,res)=>{ 
+    const Home=fs.readFileSync('../../HTML/login-mentee.html');
+    res.writeHead(200,{'Content-Type':'text/html'});
+    res.end(Home)
+})
+app.get('HTML/login-admin.html',(req,res)=>{ 
+    const Home=fs.readFileSync('../../HTML/login-admin.html');
     res.writeHead(200,{'Content-Type':'text/html'});
     res.end(Home)
 })
 app.post('/mentor_regis',(req,res)=>{
     console.log(req.body);
-    var mydata=new mentor_register(req.body)
-    mydata.save().then(()=>{
-        res.send("This item has been saved")
-    }).catch(()=>{
-        res.status(400).send("Error")
-    })
-    const mentee_registration_file=fs.readFileSync('../../HTML/card.html');
-    res.writeHead(200,{'Content-Type':'text/html'});
-    res.end(mentee_registration_file)
+    // var mydata=new mentor_register(req.body)
+    // mydata.save().then(()=>{
+    //     res.send("This item has been saved")
+    // }).catch(()=>{
+    //     res.status(400).send("Error")
+    // })
+    // const mentee_registration_file=fs.readFileSync('../../HTML/card.html');
+    // res.writeHead(200,{'Content-Type':'text/html'});
+    // res.end(mentee_registration_file)
 })
 app.post('/mentee_regis',(req,res)=>{
     console.log(req.body);
-    var mydata=new mentee_register(req.body)
-    mydata.save().then(()=>{
-        res.send("This item has been saved")
-    }).catch(()=>{
-        res.status(400).send("Error")
-    })
-    const mentee_registration_file=fs.readFileSync('../../HTML/card.html');
-    res.writeHead(200,{'Content-Type':'text/html'});
-    res.end(mentee_registration_file)
+    // var mydata=new mentee_register(req.body)
+    // mydata.save().then(()=>{
+    //     res.send("This item has been saved")
+    // }).catch(()=>{
+    //     res.status(400).send("Error")
+    // })
+    // const mentee_registration_file=fs.readFileSync('../../HTML/card.html');
+    // res.writeHead(200,{'Content-Type':'text/html'});
+    // res.end(mentee_registration_file)
 })
 app.listen(port,'127.0.0.1',()=>{
     console.log(`The application started successfully on port ${port}`);
