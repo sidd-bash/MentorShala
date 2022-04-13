676b1d173177f1d3e1f8e32dcedc0fb1f35343b3
sidd-bash <77910988+sidd-bash@users.noreply.github.com>
2022-04-13 14:42:29 +0530

    .

--- js/registration_form_data/app.js
+++ js/registration_form_data/app.js
@@ -35,46 +35,6 @@ function insertion_in_CommunityCollection(myobj) {
 }
 app.use(express.urlencoded());
 app.use(express.static(path.join(__dirname,"../../")));
-<<<<<<< HEAD
-app.get('/HTML/mentor-registration.html',(req,res)=>{ 
-    const mentor_registration_file=fs.readFileSync('../../HTML/mentor-registration.html');
-    res.writeHead(200,{'Content-Type':'text/html'});
-    res.end(mentor_registration_file)
-})
-app.get('/HTML/login-mentor.html',(req,res=>{
-    console.log(req.body);
-    const CardPage=fs.readFileSync('../../HTML/card.html');
-    res.writeHead(200,{'Content-Type':'text/html'});
-    res.end(CardPage)
-    insertion_in_personalInfo(req.body);
-}))
-app.get('/HTML/mentee-registration.html',(req,res)=>{ 
-    const mentee_registration_file=fs.readFileSync('../../HTML/mentee-registration.html');
-    res.writeHead(200,{'Content-Type':'text/html'});
-    res.end(mentee_registration_file)
-})   
-app.get('/index.html',(req,res)=>{ 
-    const Home=fs.readFileSync('index.html');
-    res.writeHead(200,{'Content-Type':'text/html'});
-    res.end(Home)
-})
-app.get('/HTML/login-mentor.html',(req,res)=>{ 
-    const loginMentor=fs.readFileSync('../../HTML/login-mentor.html');
-    res.writeHead(200,{'Content-Type':'text/html'});
-    res.end(loginMentor)
-})
-app.get('HTML/login-mentee.html',(req,res)=>{ 
-    const loginMentee=fs.readFileSync('../../HTML/login-mentee.html');
-    res.writeHead(200,{'Content-Type':'text/html'});
-    res.end(loginMentee)
-})
-app.get('HTML/login-admin.html',(req,res)=>{ 
-    const loginAdmin=fs.readFileSync('../../HTML/login-admin.html');
-    res.writeHead(200,{'Content-Type':'text/html'});
-    res.end(loginAdmin)
-})
-=======
->>>>>>> ddf1a4431f0117b30bafda59a7332e2ff6ded2b1
 app.post('/HTML/card.html',(req,res)=>{
     console.log(req.body);
     res.redirect('../../HTML/card.html')
